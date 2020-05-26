92b5b976622f5768c0d4815fdead0ce2e5044a0b
sora-uzu <naruto07170717@gmail.com>
2020-05-26 19:18:58 +0900

    [Update]chapter4

--- lib/each.rb
+++ lib/each.rb
@@ -20,6 +20,32 @@
 # non_multiples_of_three = numbers.reject { |n| n%3 == 0 }
 # puts non_multiples_of_three
 
-numbers = [1,2,3,4,5,6]
-even_number = numbers.find { |n| n.even? }
-puts even_number
+# numbers = [1,2,3,4]
+# sum = numbers.inject(0) { |result,n| result+n }
+# puts sum
+
+# def liquid?(temperature)
+#   (0...100).include?(temperature)
+# end
+
+# puts liquid?(-1)
+# puts liquid?(0)
+# puts liquid?(99)
+# puts liquid?(100)
+
+def charge(age)
+  case age
+  when 0..5
+    0
+  when 6..12
+    300
+  when 13..18
+    600
+  else
+    1000
+  end
+end
+puts charge(3)
+puts charge(12)
+puts charge(16)
+puts charge(25)
