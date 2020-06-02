f5b7d119a38f76602a417de24912208c066df4de
sora-uzu <naruto07170717@gmail.com>
2020-06-02 17:47:23 +0900

    [Update]chapter9

--- lib/chapter8.rb
+++ lib/chapter8.rb
@@ -225,16 +225,51 @@
 # user = User.new('Alice')
 # p user.shuffled_name
 
-puts 'Start.'
-module Greeter
-  def hello
-    'hello'
-  end
-end
+# puts 'Start.'
+# module Greeter
+#   def hello
+#     'hello'
+#   end
+# end
 
-begin
-greeter = Greeter.new
-rescue
-  puts '例外が発生したが、このまま実行する'
-end
-puts 'End.'
+# begin
+# greeter = Greeter.new
+# rescue
+#   puts '例外が発生したが、このまま実行する'
+# end
+# puts 'End.'
+
+# def method_1
+#     puts 'method_1 start.'
+#     method_2
+#     puts 'method_1 end.'
+#   end
+
+#   def method_2
+#     puts 'method_2 start.'
+#     method_3
+#     puts 'method_2 end.'
+#   end
+
+#   def method_3
+#     puts 'method_3 start.'
+#     1 / 0
+#     puts 'method_3 end.'
+#   end
+
+#   p method_1
+
+# retry_count = 0
+# begin
+#   puts'処理を開始します'
+#   1/0
+#   rescue
+#     retry_count += 1
+#     if retry_count <= 3
+#       puts"retryします。 (#{retry_count}回目)"
+#       retry
+#     else
+#       puts 'retryに失敗しました。'
+#     end
+#   end
+
---
+++ lib/chapter9.rb
@@ -0,0 +1,15 @@
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+    'dollar'
+  when :india
+    'rupee'
+  else
+    raise ArgumentError.new("無効な国名です。 #{country}")
+  end
+end
+
+p currency_of(:japan)
+p currency_of(:italy)
