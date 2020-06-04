7743bf808c35c45ab4057894c5eb18360aa602a6
sora-uzu <naruto07170717@gmail.com>
2020-06-04 13:39:12 +0900

    [Update]chapter10

--- lib/chapter10.rb
+++ lib/chapter10.rb
@@ -11,13 +11,101 @@
 # greeting do
 #   puts 'こんにちは'
 # end
-def greeting
+# def greeting
+#   puts 'おはよう'
+#   text = yield 'こんにちは'
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# greeting do |text|
+# text*2
+# end
+# def greeting
+#   puts 'おはよう'
+#   text = yield 'こんにちは', 12345
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# greeting do |text|
+# text*2
+# end
+
+# def greeting
+#   puts 'おはよう'
+#   text = yield 'こんにちは'
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# greeting do |text,other|
+#   text*2 + other.inspect
+# end
+
+# def greeting(&block)
+#   puts 'おはよう'
+#   unless block.nil?
+#   text = block.call('こんにちは')
+#   puts text
+#   end
+#   puts 'こんばんは'
+# end
+
+# p greeting
+
+# greeting do |text|
+#   text * 2
+# end
+# def greeting(&block)
+#   puts 'おはよう'
+#   if block_given?
+#     text = yield 'こんにちは'
+#   puts text
+#   end
+#   puts 'こんばんは'
+# end
+
+# def greeting_ja(&block)
+#   texts = ['おはよう','こんにちは','こんばんは']
+#   greeting_common(texts, &block)
+# end
+
+# def greeting_en(&block)
+#   texts = ['good morning', 'hello', 'good evening']
+#   greeting_common(texts, &block)
+# end
+
+# def greeting_common(texts, &block)
+#   puts texts[0]
+#   puts block.call(texts[1])
+#   puts texts[2]
+# end
+
+# greeting_ja do |text|
+#   text * 2
+# end
+
+# greeting_en do |text|
+#   text.upcase
+# end
+
+def greeting(&block)
   puts 'おはよう'
-  text = yield 'こんにちは'
+  text = 
+  if block.arity == 1
+    yield 'こんにちは'
+  elsif block.arity == 2
+    yield 'こんに','ちは'
+  end
   puts text
   puts 'こんばんは'
 end
 
 greeting do |text|
-text*2
+  text * 2
+end
+
+greeting do |text_1, text_2|
+  text_1 * 2 + text_2 * 2
 end
