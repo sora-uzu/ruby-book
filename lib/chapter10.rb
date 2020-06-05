312d4d5fc2491bbd4ee4526945b2109094a02cd8
sora-uzu <naruto07170717@gmail.com>
2020-06-05 20:17:12 +0900

    [Update]chapter10

--- lib/chapter10.rb
+++ lib/chapter10.rb
@@ -90,22 +90,99 @@
 #   text.upcase
 # end
 
-def greeting(&block)
-  puts 'おはよう'
-  text = 
-  if block.arity == 1
-    yield 'こんにちは'
-  elsif block.arity == 2
-    yield 'こんに','ちは'
-  end
-  puts text
-  puts 'こんばんは'
+# def greeting(&block)
+#   puts 'おはよう'
+#   text = 
+#   if block.arity == 1
+#     yield 'こんにちは'
+#   elsif block.arity == 2
+#     yield 'こんに','ちは'
+#   end
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# greeting do |text|
+#   text * 2
+# end
+
+# greeting do |text_1, text_2|
+#   text_1 * 2 + text_2 * 2
+# end
+
+# hello_proc = Proc.new do
+#   'Hello!'
+# end
+
+# def greeting(&block)
+#   puts block.class
+#   puts 'おはよう'
+#   text = block.call('こんにちは')
+#   puts text
+#   puts 'こんばんは'
+# end
+# greeting do |text|
+#   text * 2
+# end
+
+# def greeting(&block)
+#   puts 'おはよう'
+#   text = block.call('こんにちは')
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# repeat_proc = Proc.new { |text| text * 2 }
+# greeting(&repeat_proc)
+# def greeting(arrange_proc)
+#   puts 'おはよう'
+#   text = arrange_proc.call('こんにちは')
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# repeat_proc = Proc.new { |text| text * 2 }
+# greeting(repeat_proc)
+# def greeting(proc_1, proc_2, proc_3)
+#   puts proc_1.call('おはよう')
+#   puts proc_2.call('こんにちは')
+#   puts proc_3.call('こんばんは')
+# end
+
+# shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
+# repeat_proc = Proc.new { |text| text * 2 }
+# question_proc = Proc.new { |text| "#{text}?" }
+
+# greeting(shuffle_proc, repeat_proc, question_proc)
+
+# def generate_proc(array)
+#   conter = 0
+#   Proc.new do
+#     counter += 10
+#     array << counter
+#   end
+# end
+# values = []
+# sample_proc = generate_proc(values)
+# p values
+
+# sample_proc.call
+# p values
+
+# sample_proc.call
+# p values
+
+def proc_return 
+  f = Proc.new{ |n| return n*10 }
+  ret = [1,2,3].map(&f)
+  "ret: #{ret}"
 end
 
-greeting do |text|
-  text * 2
+def lambda_return
+  f = ->(n) { return n*10 }
+  ret = [1,2,3].map(&f)
+  "ret: #{ret}"
 end
 
-greeting do |text_1, text_2|
-  text_1 * 2 + text_2 * 2
-end
+p proc_return
+p lambda_return
